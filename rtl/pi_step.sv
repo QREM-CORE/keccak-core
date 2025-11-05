ad7528a886a18f4c8ed398b8dfaaed731baefadc
Kiet Le <ktlbusiness11@gmail.com>
2025-11-05 18:31:01 -0500

    PI: Start Module

---
+++ rtl/pi_step.sv
@@ -0,0 +1,17 @@
+/*
+ * Module Name: pi_step
+ * Author: Kiet Le
+ * Description: The effect of π is to rearrange the positions of the lanes.
+ * NOTE: Purely combinational so far. Can be pipelined for higher clock speed if needed.
+ */
+
+import keccak_pkg::*;
+
+module rho_step (
+    input   [ROW_SIZE-1:0][COL_SIZE-1:0][LANE_SIZE-1:0] state_array_in,
+    output  [ROW_SIZE-1:0][COL_SIZE-1:0][LANE_SIZE-1:0] state_array_out
+);
+
+    
+
+endmodule
